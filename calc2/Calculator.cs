721af54cd0fb5bff5eb2c6fb761d1173229c47f1
s7ez <s7eeez@gmail.com>
2024-03-31 00:26:30 +0900

    :bee:

--- calc2/Calculator.cs
+++ calc2/Calculator.cs
@@ -11,25 +11,34 @@ namespace calc2 {
     internal class Calculator : NotifyPropertyChanged {
         public string Text { get; private set; } = "";
         public char Sign { get; private set; } = '+';
+        void SetSign(char newSign) => Sign = newSign;
+
+        readonly string[] nstrs = new string[2];
+        bool isSelectedN1 = true;
+
+        public ICommand Backspace => new DelegateCommand(() => {
+            int idx = isSelectedN1 ? 1 : 0,
+                len = nstrs[idx].Length;
+            List<char> preList = [.. nstrs[idx]];
+            preList.RemoveAt(len);
+            nstrs[idx] = preList.ToString();
+        });
 
         public ICommand ButtonClear => new DelegateCommand(() => Text = "");
 
         public ICommand Button1 => new DelegateCommand(() => {
-            nstrs[isN1 ? 0 : 1] += "1";
-            isN1 = !isN1;
-            Text += string.Join(Sign, nstrs); // "1"; // $"{nstrs[0]}:{nstrs[1]}";
+            nstrs[isSelectedN1 ? 0 : 1] += "1";
+            isSelectedN1 = !isSelectedN1;
+            Text = string.Join(Sign, nstrs);
         });
-
-        readonly string[] nstrs = new string[2];
-        bool isN1 = true;
 
         public ICommand DoCalculate => new DelegateCommand(() => {
             double value0 = double.Parse(nstrs[0]), value1 = double.Parse(nstrs[1]);
             double dst = Sign switch {
                 '+' => value0 + value1,
                 '-' => value0 - value1,
-                '×' => value0 * value1,
-                '÷' => value0 / value1,
+                '×' or '*' => value0 * value1,
+                '÷' or '/' => value0 / value1,
                 _ => throw new Exception()
             };
             Text = dst.ToString();
---
+++ calc2/Program.cs
@@ -0,0 +1,16 @@
+﻿const int idx = 0;
+
+string[] texts = ["12345", "67890"];
+Console.WriteLine("debug: " + string.Join('+', texts) + '=' + (double.Parse(texts[0]) + double.Parse(texts[1])));
+
+int length = texts[idx].Length;
+Console.WriteLine("list length: " + length);
+
+List<char> preList = [.. texts[idx]];
+Console.WriteLine("preList: " + string.Join(null, preList));
+
+preList.RemoveAt(length - 1);
+Console.WriteLine("afterList: " + string.Join(null, preList));
+
+texts[idx] = string.Join(null, preList);
+Console.WriteLine("final: " + string.Join('+', texts) + '=' + (double.Parse(texts[0]) + double.Parse(texts[1])));
